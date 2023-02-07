66d59d4ab207042f904ed61ef578a252ca7db5fb
German Horianski <germanezequielhorianski@gmail.com>
2023-02-06 22:30:32 -0300

    Add a ImageBackground

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
+import { ImageBackground, StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
@@ -9,14 +9,24 @@ export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
+  const image = require('./assets/images/starry_night.jpeg')
+
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <ImageBackground source={image} style={styles.image}>
+          <Navigation colorScheme={colorScheme} />
+        </ImageBackground>
         <StatusBar />
       </SafeAreaProvider>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  image: {
+    flex:1
+  }
+})
